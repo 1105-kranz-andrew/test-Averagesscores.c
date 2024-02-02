de99fe4e9ea03ab9abf7baf0ff23939cbc095c66
1105-kranz-andrew <157994922+1105-kranz-andrew@users.noreply.github.com>
2024-02-01 19:52:10 -0800

    Create test score averages.c

---
+++ test score averages.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+  int main() {
+      double Test 1 = 88.88, Test 2 = 99.92
+    sum= Test  1+ Test 2 ; 
+    average = sum/2 
+      printf ("Exam Average: %.2lf/n, average) ;
+        return 0;
+      }
